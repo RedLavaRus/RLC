0dcf26289baa5fd2b0dc086e03e2d9e43f78bdcf
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-01-03 18:30:28 +0300

    class loader

---
+++ rlc.test/index.php
@@ -0,0 +1,16 @@
+<?php
+
+define("MYPOS" , __DIR__);
+define("SLASH" ,"\\");
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+spl_autoload_register(function ($class_name) {   
+    $class_name = str_replace('\\','/',$class_name);
+    $class_name = strtolower($class_name);
+    include_once MYPOS."/".$class_name . '.php';    
+});
+
+?>
