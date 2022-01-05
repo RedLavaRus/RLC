783bb8ad66b698079a33e9fa2413b34908796a7a
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-01-05 12:09:34 +0300

    add user skelete

---
+++ rlc.test/mod/user/description.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mod\User;
+
+Class Description{
+    public $author = "JaligWei";
+    public $company = "RedLava";
+    public $url = "RedLava.ru";
+    public $version = "0.1";
+    public $name = "User";
+    public $compatibility = "1.0.1";
+    public $ico = "ico.png";
+    public $imf = "img.png";
+    public $description = "Управление пользователями";
+
+}
---
+++ rlc.test/mod/user/user.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Mod\User;
+
+Class User{
+   
+    public function register(){
+
+    }
+   
+    public function auth(){
+        
+    }
+   
+    public function exit(){
+        
+    }
+   
+    public function recovery(){
+        
+    }
+}
