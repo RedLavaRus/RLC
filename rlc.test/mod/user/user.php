8ed45627bef3b3f73b9c06e67ad958ffa1ebf2da
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-01-06 00:25:18 +0300

    Add cheaking user

--- rlc.test/mod/user/user.php
+++ rlc.test/mod/user/user.php
@@ -32,6 +32,7 @@ Class User{
         $login = "21312312";
         $pass1 = "21312312";
         $pass2 = "21312312";
+        $email = "test@test.ru";
         //проверить совпадают ли пароли
         $this->checking_uniformity($pass1, $pass2);
         //Проверить соответствует ли длина логина
@@ -39,6 +40,7 @@ Class User{
         //проверить соответсвует ли длина пароля
         $this->length_check("pass",$pass1);
         //проверить релевантность емаила
+        $this->checking_email($email);
         //проверка на ошибки
     }
    
@@ -66,11 +68,33 @@ Class User{
     public function length_check($type,$var){
         $cfg = new \Mod\User\Config;
         if($type == "login"){
-
+            if(strlen($var) > $cfg->max_login){
+                $e["active"] = "act";
+                $e["login"] = "Максимальная длинна логина ".$cfg->max_login." символов.";
+            }
+            if(strlen($var) < $cfg->min_login){
+                $e["active"] = "act";
+                $e["login"] = "Минимальная длинна логина ".$cfg->min_login." символов.";
+            }
         }
         if($type == "pass"){
+            if(strlen($var) > $cfg->max_pass){
+                $e["active"] = "act";
+                $e["pass1"] = "Максимальная длинна пароля ".$cfg->max_pass." символов.";
+            }
+            if(strlen($var) < $cfg->min_pass){
+                $e["active"] = "act";
+                $e["pass1"] = "Минимальная длинна пароля ".$cfg->min_pass." символов.";
+            }
 
         }
 
+    }
+    
+    public function checking_email($email){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $e["active"] = "act";
+            $e["email"] = "Неверная почта";
+        }
     }
 }
