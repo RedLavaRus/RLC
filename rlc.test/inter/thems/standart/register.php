11b9555dcfd78d710b0f15e73d0d0d3c9204bc21
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-01-09 19:57:41 +0300

    thems fix

--- rlc.test/inter/thems/standart/register.php
+++ rlc.test/inter/thems/standart/register.php
@@ -4,43 +4,46 @@
         <div class="register_title">Регистрация</div>
 
         <div class="register_error">
-            Не верный логин
+        <?php if(isset($login)){ echo $login;}else {echo "&nbsp;";}?>
         </div>
         <p>
-            <input class="register_username" id="username" name="username" required="required" type="text" placeholder="Имя" />
+            <input class="register_username" id="username" name="login" required="required" type="text" placeholder="Имя" />
         </p>
 
-        <div class="register_error">Не верный логин
+        <div class="register_error">
+        <?php if(isset($pass1)){ echo $pass1;}else {echo "&nbsp;";}?>
         </div>
         <p>
-            <input class="register_password" id="password" name="password" required="required" type="password" placeholder="Пароль" />
+            <input class="register_password" id="password" name="password1" required="required" type="password" placeholder="Пароль" />
         </p>
 
-        <div class="register_error">&nbsp;
+        <div class="register_error">
+        <?php if(isset($pass1)){ echo $pass1;}else {echo "&nbsp;";}?>
         </div>
         <p>
-            <input class="register_password" id="password" name="password" required="required" type="password" placeholder="Повторите пароль" />
+            <input class="register_password" id="password" name="password2" required="required" type="password" placeholder="Повторите пароль" />
         </p>
 
-        <div class="register_error">&nbsp;
+        <div class="register_error">
+        <?php if(isset($email)){ echo $email;}else {echo "&nbsp;";}?>
         </div>
         <p>
             <input class="register_password" id="email" name="email" required="required" type="email" placeholder="E-mail" />
         </p>
         <p class="keep_register">
-            <input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" />
+            <input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping"  required="required"/>
             <label for="loginkeeping">Даю согласие на обработку персональных данных</label>
         </p>
-
+<?php /*
         <div class="register_captcha">
             <img class="register_captcha_img" src="img/captcha-internet-translate.jpg" alt="">
             <input class="register_captcha_input" id="captcha" name="captcha" required="required" type="captcha" placeholder=" Введите текст с картинки" />
         </div>
-
+*/ ?>
         <div class="register_box_button">
             <a class="register_button_login" href="">Вход</a>
             <p class="register_button">
-                <input class="register_button_input" type="submit" value="Далее" />
+                <input class="register_button_input" name="submit" type="submit" value="Далее" />
             </p>
         </div>
     </div>
