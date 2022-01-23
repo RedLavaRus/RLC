0b963b66f206ec3d308cc56f56785532e3d58114
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-01-23 23:29:52 +0300

    fix

--- rlc.test/inter/thems/standart/headadmin.php
+++ rlc.test/inter/thems/standart/headadmin.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="/src/css/styleadmin.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>    
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 </head>
