459fdf9d42248f42bfaba921621c70130d960f6d
RedLavaRus <57762436+RedLavaRus@users.noreply.github.com>
2022-02-04 00:47:47 +0300

    up blog

--- rlc.test/mod/blog/index.php
+++ rlc.test/mod/blog/index.php
@@ -9,15 +9,10 @@ Class Index{
     }
     public function show_news_all(){
 
-        $position = 0;
-        $sql = new \Mod\Sql\Sql;
-        $connect = $sql->db_connect;
-            $sth = $connect->prepare("SELECT * FROM `news` WHERE `shows` = 'yes' LIMIT 10 OFFSET ".$position." ");// WHERE `shows` = 'yes' LIMIT 10 OFFSET ?");
-            $sth->execute(array($position));
-        
-            $bd = null;
-        while($result_sql = $sth->fetch(\PDO::FETCH_ASSOC)){
-            $bd[] = $result_sql;
+       // echo ">>>>>>>>>>>>>".$this->num_page_news();
+        $db = $this->lust_page_news($this->num_page_news());
+        if($db == null){
+            $this->e404();
         }
         
 
@@ -32,7 +27,7 @@ Class Index{
         $page3[]="footer";
         $view = new \Mod\View\View;
         $view->show($page);
-        $view->view_line($dir,$page1,$bd);
+        $view->view_line($dir,$page1,$db);
         $view->show($page3);
     }
     public function show_news_one(){
@@ -75,5 +70,21 @@ Class Index{
         while($result_sql = $sth->fetch(\PDO::FETCH_ASSOC)){
             $bd[] = $result_sql;
         }
+        return $bd;
+    }
+
+    public function num_page_news(){
+        
+        if(isset($_GET["page"]) and $_GET["page"] >1 ) {
+            $page = $_GET["page"];
+        }
+        else{
+            $page = 1;
+        }
+        return $page;
+    }
+    public function e404(){
+        echo "error 404: page don't faunt!!!";
+        die();
     }
 }
